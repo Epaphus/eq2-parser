13e0a29ecce8cacfd833b8fdaba07940fcaf3455
David Wilkinson <git@noroutetohost.net>
2013-05-06 18:11:10 +0100

    Added in some more items to pull out from the log, Chat messages, people joing/leaving, loots, rolls, etc

--- config.rb
+++ config.rb
@@ -52,6 +52,75 @@ def checkline (line=line)
     parts = line.match(regexline)
     dooutput "Time to power feed - #{parts['Player']}"
 
+# Just some ramdom message stuff 
+
+#Record zone changes
+  when /.*? You have entered (?<Zone>.+?)\..*/
+    regexline = /.*? You have entered (?<Zone>.+?)\..*/
+    parts = line.match(regexline)
+    dooutput "Zoned in to - #{parts['Zone']}"
+    
+#Chat Messages
+  when /.*?aPC -1 (?<Player>.+?):(?<Player2>.+?)\\\/a (says|say) to the (?<Where>.+?), \"(?<Text>.+?)\".*/    
+  regexline = /.*?aPC -1 (?<Player>.+?):(?<Player2>.+?)\\\/a (says|say) to the (?<Where>.+?), \"(?<Text>.+?)\".*/
+  parts = line.match(regexline)
+  dooutput "#{parts['Where'].capitalize}: #{parts['Player']} - #{parts['Text']}"  
+
+#You say Messages
+  when /.*?\] (?<Player>.+?) say to the (?<Where>.+?), \"(?<Text>.+?)\".*/    
+  regexline = /.*?\] (?<Player>.+?) say to the (?<Where>.+?), \"(?<Text>.+?)\".*/
+  parts = line.match(regexline)
+  dooutput "#{parts['Where'].capitalize}: #{parts['Player']} - #{parts['Text']}"  
+  
+# Player Joined Group/Raid
+  when /.*?\] (?<Player>.+?) has joined the(?<Text>.+?)\..*/
+    regexline = /.*?\] (?<Player>.+?) has joined the(?<Text>.+?)\..*/
+    parts = line.match(regexline)
+    dooutput "#{parts['Player']} Joined #{parts['Text']}"
+    
+# Player Left Group/Raid     
+  when /.*?\] (?<Player>.+?) left the (?<Text>.+?)\..*/
+    regexline = /.*?\] (?<Player>.+?) left the (?<Text>.+?)\..*/
+    parts = line.match(regexline)
+    dooutput "#{parts['Player']} left #{parts['Text']}"
+    
+# Memember logged in/out    
+  when /.*?Guildmate: (?<Player>.+?) has logged (?<Text>.+?)\..*/
+    regexline = /.*?Guildmate: (?<Player>.+?) has logged (?<Text>.+?)\..*/  
+    parts = line.match(regexline)
+    dooutput "Guild Member #{parts['Player']} Logged #{parts['Text']}"    
+    
+# earned the achievement
+  when /.*?\] (?<Player>.+?) earned the achievement \'(?<Text>.+?)\'.*/
+    regexline = /.*?\] (?<Player>.+?) earned the achievement \'(?<Text>.+?)\'.*/  
+    parts = line.match(regexline)
+    dooutput "#{parts['Player']} earned the achievement #{parts['Text']}"    
+    
+# Looted item (Guild Chat)    
+  when /.*?\] (?<Player>.+?) looted the Fabled .aITEM (?<itemid1>.+?) (?<itemid2>.+?):(?<Text>.+?)\\\/a..*/
+    regexline = /.*?\] (?<Player>.+?) looted the Fabled .aITEM (?<itemid1>.+?) (?<itemid2>.+?):(?<Text>.+?)\\\/a..*/  
+    parts = line.match(regexline)
+    dooutput "Guild: #{parts['Player']} Looted #{parts['Text']}"    
+
+# Looted item (Local chat)        
+  when /.*?\] (?<Player>.+?) loots .aITEM (?<itemid1>.+?) (?<itemid2>.+?):(?<Text>.+?)\\\/a from the (?<Chest>.+?) of (?<Mob>.+?)\..*/
+    regexline = /.*?\] (?<Player>.+?) loots .aITEM (?<itemid1>.+?) (?<itemid2>.+?):(?<Text>.+?)\\\/a from the (?<Chest>.+?) of (?<Mob>.+?)\..*/  
+    parts = line.match(regexline)
+    dooutput "Loot: #{parts['Player']} Looted #{parts['Text']} from the #{parts['Chest']} of #{parts['Mob']}"      
+
+# Dice Random        
+  when /.*? Random:(?<Player>.+?) rolls from (?<From>.+?) to (?<To>.+?) on the magic dice...and scores a (?<Score>.+?)\!.*/
+    regexline = /.*? Random:(?<Player>.+?) rolls from (?<From>.+?) to (?<To>.+?) on the magic dice...and scores a (?<Score>.+?)\!.*/  
+    parts = line.match(regexline)
+    dooutput "Random: #{parts['Player']} Rolls #{parts['To']} and scores #{parts['Score']}"    
+        
+        
+#  when / /
+#    regexline = / /  
+#    parts = line.match(regexline)
+#    dooutput "Guild: #{parts['Player']} in to - #{parts['Text']}"    
+    
+# What to do if no match? Right now nothing other than count it    
   else
 
   @linesnotmatched += 1
